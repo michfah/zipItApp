b72378dbfd96b9b92e7eb55d56cde3b370a8f4ce
michfah <michfah85@yahoo.com>
2017-04-25 11:21:58 +0100

    Update zipsFactory.js

--- app/services/zipsFactory.js
+++ app/services/zipsFactory.js
@@ -7,13 +7,18 @@ angular
         function getZips() {
             return $http.get('/places');
         };
+    
+        function getRegionMap() {
+            return $http.get('/regionMap');
+        };
         
         function getMap(zipId) {
-            return $http.get('/map/' + zipId);
+            return $http.get('/map/'+ zipId);
         };
         
         return { 
             getZips: getZips,
+            getRegionMap: getRegionMap,
             getMap: getMap
         }
     });
